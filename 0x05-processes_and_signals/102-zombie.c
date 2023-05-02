72b9c7c31472985f2e87fd4b721bd8fb949c5494
haileyesusalx <kefyalewhaileyesusalx@gmail.com>
2023-05-01 17:18:16 -0700

    task 11

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,53 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+
+/**
+ * infinite_while - infinite while loop
+ *
+ * Return: Always 0.
+ */
+int infinite_while(void)
+{
+    while (1)
+    {
+        sleep(1);
+    }
+    return (0);
+}
+
+/**
+ * main - creates 5 zombie processes
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int i;
+    pid_t child_pid;
+
+    for (i = 0; i < 5; i++)
+    {
+        child_pid = fork();
+        if (child_pid == 0)
+        {
+            /* Child process */
+            exit(0);
+        }
+        else if (child_pid > 0)
+        {
+            /* Parent process */
+            printf("Zombie process created, PID: %d\n", child_pid);
+        }
+        else
+        {
+            /* Error occurred */
+            perror("fork");
+            exit(1);
+        }
+    }
+
+    infinite_while();
+
+    return (0);
+}
