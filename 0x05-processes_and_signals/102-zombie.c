8c2d506e564798ebaf3e148dca6cc02e9bae74f7
haileyesusalx <kefyalewhaileyesusalx@gmail.com>
2023-05-01 17:35:44 -0700

    task 11

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -9,11 +9,11 @@
  */
 int infinite_while(void)
 {
-    while (1)
-    {
-        sleep(1);
-    }
-    return (0);
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
 }
 
 /**
@@ -23,31 +23,29 @@ int infinite_while(void)
  */
 int main(void)
 {
-    int i;
-    pid_t child_pid;
+	int i;
+	pid_t child_pid;
 
-    for (i = 0; i < 5; i++)
-    {
-        child_pid = fork();
-        if (child_pid == 0)
-        {
-            /* Child process */
-            exit(0);
-        }
-        else if (child_pid > 0)
-        {
-            /* Parent process */
-            printf("Zombie process created, PID: %d\n", child_pid);
-        }
-        else
-        {
-            /* Error occurred */
-            perror("fork");
-            exit(1);
-        }
-    }
-
-    infinite_while();
-
-    return (0);
+	for (i = 0; i < 5; i++)
+	{
+		child_pid = fork();
+		if (child_pid == 0)
+		{
+			/* Child process */
+			exit(0);
+		}
+		else if (child_pid > 0)
+		{
+			/* Parent process */
+			printf("Zombie process created, PID: %d\n", child_pid);
+		}
+		else
+		{
+			/* Error occurred */
+			perror("fork");
+			exit(1);
+		}
+	}
+	infinite_while();
+	return (0);
 }
