b9886c88036ca62621908534e9c1abbe2a7fe77f
haileyesusalx <kefyalewhaileyesusalx@gmail.com>
2023-05-05 15:46:05 -0700

    task 8

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,13 +1,23 @@
 #!/usr/bin/env ruby
+import re
 
-log_file = ARGV[0]
+# Define regular expressions to match the relevant fields in the log lines
+sender_pattern = r"\[from:([^]]+)\]"
+receiver_pattern = r"\[to:([^]]+)\]"
+flags_pattern = r"\[flags:([^]]+)\]"
 
-File.readlines(log_file).each do |line|
-  match_data = line.match(/\[from:(?<sender>.*)\] \[to:(?<receiver>.*)\] \[flags:(?<flags>.*)\]/)
-  if match_data
-    sender = match_data[:sender]
-    receiver = match_data[:receiver]
-    flags = match_data[:flags]
-    puts "#{sender},#{receiver},#{flags}"
-  end
-end
+# Open the log file for reading
+with open("logfile.txt", "r") as f:
+    # Iterate over each line in the file
+    for line in f:
+        # Use regular expressions to extract the sender, receiver, and flags
+        sender_match = re.search(sender_pattern, line)
+        receiver_match = re.search(receiver_pattern, line)
+        flags_match = re.search(flags_pattern, line)
+
+        # If all three fields were found, print them in the required format
+        if sender_match and receiver_match and flags_match:
+            sender = sender_match.group(1)
+            receiver = receiver_match.group(1)
+            flags = flags_match.group(1)
+            print(f"{sender},{receiver},{flags}")
