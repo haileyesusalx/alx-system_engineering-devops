10a6a07cdf52b08c98142d42d97514d7b2a36953
haileyesusalx <kefyalewhaileyesusalx@gmail.com>
2023-10-12 12:08:01 -0400

    task 0 and READme file

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,8 +1,21 @@
 #!/usr/bin/python3
+"""
+This script queries the Reddit API and returns the number of subscribers (not active users, total subscribers) for a given subreddit.
+If the subreddit is invalid, it returns 0.
+"""
+
 import requests
 
-
 def number_of_subscribers(subreddit):
+    """
+    Get the number of subscribers for a given subreddit.
+    
+    Args:
+        subreddit (str): The name of the subreddit to query.
+
+    Returns:
+        int: The number of subscribers for the subreddit, or 0 if invalid.
+    """
     # Define the Reddit API endpoint
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
 
@@ -20,7 +33,6 @@ def number_of_subscribers(subreddit):
     else:
         # Return 0 for invalid or non-existent subreddits
         return 0
-
 
 if __name__ == '__main__':
     import sys
